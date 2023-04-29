06fb017d08738e0ed7bbfd59e6fc8722ba781b7c
localmachine <localmachine@Airlocalmachine.Dlink>
2023-04-29 18:26:12 +0300

    api.users.tests.test_user_flow & test_user_flow_correct

--- api/conftest.py
+++ api/conftest.py
@@ -39,8 +39,8 @@ ADMIN_NAME = 'admin'
 
 @pytest.fixture
 def admin():
-    # TODO нужно дополнить фикстуру что бы она возвращала вновь созданного пользователя в статусе admin
-    ...
+    admin = User.objects.create_superuser(username=ADMIN_NAME)
+    return admin
 
 
 @pytest.fixture
--- api/users/tests.py
+++ api/users/tests.py
@@ -2,6 +2,7 @@ import json
 from typing import TYPE_CHECKING
 
 import pytest
+from django.contrib.auth.models import User as user_model
 
 if TYPE_CHECKING:
     from django.contrib.auth.models import User
@@ -35,34 +36,125 @@ def test_auth_using_login_pass(anon_client: 'APIClient', user_with_password: 'Us
 
     assert data['username'] == username
 
+# Задание поставило меня в ступор, где было два пути.
+# Первый - написать тесты по заданию.
+# Второй - написать рабочие тесты исходя из ресурсов проекта.
+# Я решил идти по обоим :) Тесты представлены, соответственно списку выше.
 
 @pytest.mark.django_db
 def test_user_flow(admin_client: 'APIClient', anon_client: 'APIClient'):
-    """
-    TODO Дополните тест.
-     Требуется подготовить данные для создания нескольких рандомных пользователей используя клиент для admin
-     (не забудьте вначале починить фикстуру admin) и выполнить следующие действия:
-     1. Создать пользователя отправив POST-запрос на url '/api/v1/users/'
-        response.status_code == 200
-     2. Проверить количество созданных пользователей отправив GET-запрос на url '/api/v1/users/'
-        response.status_code == 200
-     3. Проверить авторизацию для каждого нового пользователя. Необходимо используя анонимный клиент
-        отправить POST-запрос на url f'/api/v1/users/{created_users_id}/' response.status_code == 200
-     4. Удалить всех созданных пользователей, отправив DELETE-запрос на url f'/api/v1/users/{created_users_id}/',
-        response.status_code == 204
+    """Тесты по TODO."""
 
-    Пример создания данных для рандомных пользователей.
+    valid_username = 'user_{}'
+    valid_password = 'password_{}'
+    valid_email = 'email_{}@test.task'
 
-    users_count = 20
-    users_data = [
-        {
-            'username': f'user_{i}',
-            'password': f'password_{i}',
-            'email': f'email_{i}@mail.ru',
+    users_count = 10
+    valid_users_data = {
+        valid_username.format(i): {
+            'username': valid_username.format(i),
+            'password': valid_password.format(i),
+            'email': valid_email.format(i)
         }
-        for i in range(users_count)
-    ]
+        for i in range(1, users_count + 1)
+    }
 
-    """
+    for valid_user in valid_users_data.values():
 
-    ...
+        response = admin_client.post('/api/v1/users/', data=valid_user)
+
+        assert response.status_code == 200
+
+    response = admin_client.get('/api/v1/users/')
+
+    assert response.status_code == 200
+
+    users_response_data = response.json()
+
+    assert users_response_data.get('count') == users_count
+
+    users_response_data = users_response_data.get('results')
+
+    for user in users_response_data:
+
+        user_password = valid_users_data[user.get('username')].get('password')
+
+        response = anon_client.post(
+            '/api/v1/users/{}/'.format(user.get('id')),
+            data={
+                'username': user.get('username'),
+                'password': user_password
+            }
+        )
+        assert response.status_code == 200, response.content
+
+    for user in users_response_data:
+
+        response = admin_client.delete('/api/v1/users/{}/'.format(user.get('id')))
+
+        assert response.status_code == 204
+
+
+
+@pytest.mark.django_db
+def test_user_flow_correct(admin_client: 'APIClient', anon_client: 'APIClient'):
+    """Тесты по рабочим эндпоинтам."""
+
+    valid_username = 'user_{}'
+    valid_password = 'password_{}'
+    valid_email = 'email_{}@test.task'
+
+    users_count = 10
+    valid_users_data = {
+        valid_username.format(i): {
+            'username': valid_username.format(i),
+            'password': valid_password.format(i),
+            'email': valid_email.format(i)
+        }
+        for i in range(1, users_count + 1)
+    }
+    users_count_before = user_model.objects.count()
+
+    for valid_user in valid_users_data.values():
+
+        response = admin_client.post('/api/v1/users/', data=valid_user)
+
+        # метод create в UserSerializer не отдаёт статус 200 :)
+        assert response.status_code in (200, 201)
+
+    response = admin_client.get('/api/v1/users/')
+
+    assert response.status_code == 200
+
+    users_response_data = response.json()
+
+    users_count_after_create = user_model.objects.count() - users_count_before
+
+    assert users_response_data.get('count') == users_count == users_count_after_create
+
+    users_response_data = users_response_data.get('results')
+
+    for user in users_response_data:
+
+        user_password = valid_users_data[user.get('username')].get('password')
+
+        # 1. Эндпоинт api/v1/users/{user.id} доступен только админу.
+        # 2. Этот эндпоинт не настроен на проверку авторизации.
+        response = anon_client.post(
+            '/api/auth/login/',
+            data={
+                'username': user.get('username'),
+                'password': user_password
+            }
+        )
+        assert response.status_code == 200, response.content
+
+    users_count_before = user_model.objects.count()
+
+    for user in users_response_data:
+
+        response = admin_client.delete('/api/v1/users/{}/'.format(user.get('id')))
+
+        assert response.status_code == 204
+
+    assert user_model.objects.count() == users_count_before - users_count
