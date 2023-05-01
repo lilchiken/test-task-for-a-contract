9b766202b23416c2ece0c93ca1acf27ea30d36cf
localmachine <localmachine@Airlocalmachine.Dlink>
2023-05-02 02:45:08 +0300

    front tests

---
+++ gui_tests/pages/base_page.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+class BasePage:
+
+    def __init__(self, driver, wait) -> None:
+        # Нотация хрома, как пример, webdriver -
+        # родительский класс не имеет методов
+        self.driver: webdriver.Chrome = driver
+        self.wait: WebDriverWait = wait
+
+    def goto(self, url: str):
+        self.driver.get(url)
+
+    def get_title(self):
+        return self.driver.title
